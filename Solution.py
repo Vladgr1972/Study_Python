39be71e8a730c5c93147724068b8eed8cd6b552f
Vladgr1972 <95755163+Vladgr1972@users.noreply.github.com>
2021-12-08 19:50:48 +0700

    Add files via upload
    The first version

---
+++ Solution.py
@@ -0,0 +1,10 @@
+import os
+class file:
+   def __init__(self,path_to_file):
+       self.path_to_file = path_to_file
+
+   def read(self,path_to_file):
+       pass
+
+   def write(self,path_to_file):
+       pass
