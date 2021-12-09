15a92e55f428eb88aff97db1364d416f5eee2b5e
Vladgr1972 <95755163+Vladgr1972@users.noreply.github.com>
2021-12-09 22:18:40 +0700

    Update Solution.py

--- Solution.py
+++ Solution.py
@@ -1,15 +1,29 @@
 import os
+import sys
 import tempfile
+
+
 
 class File:
     def __init__(self,path_to_file):
         self.path_to_file = self.chek_file(path_to_file)
 
-    def read(self,path_to_file):
-        pass
 
-    def write(self,path_to_file):
-        pass
+    def read(self):
+        if os.path.isfile(self.path_to_file):
+            with open(self.path_to_file, "r",encoding="utf-8") as f:
+                text = f.read()
+        else:
+            text = ""
+        return text
+
+
+    def write(self,string):
+        if os.path.isfile(self.path_to_file):
+            with open(self.path_to_file, "w",encoding="utf-8") as f:
+                f.write(string)
+
+
 
     def chek_file(self,path_to_file):
         self.path_to_file = path_to_file;
@@ -19,12 +33,16 @@ class File:
             open(file_name,"x")
         return path_to_file
 
+
     def __str__(self):
-        return  self.path_to_file
+        return  os.path.abspath(self.path_to_file)
 
 
 file_name = "text.tmp"
 file_obj = File(file_name)
 print(os.path.exists(file_name))
 print(file_obj)
+print(file_obj.read())
+file_obj.write("Some text")
+print(file_obj.read())
 
