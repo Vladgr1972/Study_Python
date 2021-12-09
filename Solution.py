9700930129c5334ab5074fbebb6f6f81a93e3697
Vladgr1972 <95755163+Vladgr1972@users.noreply.github.com>
2021-12-09 17:32:55 +0700

    Update Solution.py

--- Solution.py
+++ Solution.py
@@ -1,10 +1,27 @@
 import os
-class file:
-   def __init__(self,path_to_file):
-       self.path_to_file = path_to_file
+import tempfile
 
-   def read(self,path_to_file):
-       pass
+class File:
+    def __init__(self,path_to_file):
+        self.path_to_file = self.chek_file(path_to_file)
 
-   def write(self,path_to_file):
-       pass
+    def read(self,path_to_file):
+        pass
+
+    def write(self,path_to_file):
+        pass
+
+    def chek_file(self,path_to_file):
+        self.path_to_file = path_to_file;
+        if os.path.exists(file_name):
+            pass
+        else:
+            open(file_name,"x")
+        return path_to_file
+
+
+
+file_name = "text.tmp"
+file_obj = File(file_name)
+print(os.path.exists(file_name))
+
