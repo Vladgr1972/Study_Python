c43d60f3092827280d6fec8e66202f44b7afdef7
Vladgr1972 <95755163+Vladgr1972@users.noreply.github.com>
2021-12-10 00:17:43 +0700

    Update Solution.py

--- Solution.py
+++ Solution.py
@@ -8,7 +8,6 @@ class File:
     def __init__(self,path_to_file):
         self.path_to_file = self.chek_file(path_to_file)
 
-
     def read(self):
         if os.path.isfile(self.path_to_file):
             with open(self.path_to_file, "r",encoding="utf-8") as f:
@@ -17,14 +16,11 @@ class File:
             text = ""
         return text
 
-
     def write(self,string):
         if os.path.isfile(self.path_to_file):
             with open(self.path_to_file, "w",encoding="utf-8") as f:
                 f.write(string)
         return len(string)
-
-
 
     def chek_file(self,path_to_file):
         self.path_to_file = path_to_file;
@@ -34,20 +30,30 @@ class File:
             open(self.path_to_file,"x")
         return self.path_to_file
 
-
     def __str__(self):
         return  os.path.abspath(self.path_to_file)
 
+    def __add__(self, other):
+        sum_txt = self.read() + other.read()
+        tmp_file_dir = tempfile.gettempdir()
+        tmp_file = self.chek_file(os.path.join(tmp_file_dir, self.path_to_file + "_" + other.path_to_file))
+        with open(tmp_file,"w",encoding="utf-8") as f:
+            f.write(sum_txt)
+        return File(tmp_file)
+
+
+
 
 file_name = "text"
-
-print(os.path.exists(file_name + "_1"))
 file_obj_1 = File(file_name + "_1")
-print(os.path.exists(file_name + "_1"))
 file_obj_2 = File(file_name + "_2")
-print(file_obj_1.write('trhyrtrth'))
-print(file_obj_1)
+print(file_obj_1.write('line 1\n'))
 print(file_obj_1.read())
-print(file_obj_2.write('l55y5'))
-print(file_obj_2)
+print(file_obj_2.write('line 2\n'))
 print(file_obj_2.read())
+print(type(file_obj_1))
+new_file_obj = file_obj_1 + file_obj_2
+print(new_file_obj)
+print(type(new_file_obj))
+print(new_file_obj)
+
